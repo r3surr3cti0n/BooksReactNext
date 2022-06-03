2a14ba8808ced31cf6e9c90d05116644fa4da7cd
r3surr3cti0n <r3surr3cti0n@protonmail.com>
2022-06-03 16:06:12 +0200

    Fix books.map error, display Feedback

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,17 @@ import React from "react";
 import Book from "../components/SingleBook";
 import { API_URL_GET } from "../helper";
 import SharedLayout from "../components/SharedLayout";
+import Feedback from "../components/Feedback";
 
 export default function Home({ books }) {
 	return (
 		<SharedLayout isHome>
 			<ul id="books">
-				{books.map((book) => (
-					<Book key={book.id} book={book} />
-				))}
+				{books.message ? (
+					<Feedback message={books.message} />
+				) : (
+					books.map((book) => <Book key={book.id} book={book} />)
+				)}
 			</ul>
 		</SharedLayout>
 	);
