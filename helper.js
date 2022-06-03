51664d282d1ebb964ccc478de33357700202fa9e
r3surr3cti0n <r3surr3cti0n@protonmail.com>
2022-06-03 15:27:13 +0200

    Change URLs to heroku

--- helper.js
+++ helper.js
@@ -1,8 +1,11 @@
-const API_URL = "http://localhost:3001/api/book";
+// const API_URL_GET = "http://localhost:3001/api/get/book";
+// const API_URL_PUT = "http://localhost:3001/api/put/book";
+const API_URL_GET = "https://books-api-react.herokuapp.com/api/get/book";
+const API_URL_PUT = "https://books-api-react.herokuapp.com/api/put/book";
 
 // Return only book IDs
 const getAllBookIds = async () => {
-	const res = await fetch(API_URL).then((res) => res.json());
+	const res = await fetch(API_URL_GET).then((res) => res.json());
 	const books = res.map((book) => {
 		return {
 			id: String(book.id),
@@ -12,4 +15,4 @@ const getAllBookIds = async () => {
 	return books;
 };
 
-export { API_URL, getAllBookIds };
+export { API_URL_GET, API_URL_PUT, getAllBookIds };
